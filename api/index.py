d333a4539a244ca6da50c1cae1e5e6f54ed7ec7a
Frankie Willard <69867065+frankwillard@users.noreply.github.com>
2023-08-14 18:17:06 -0400

    Put everything in serverless function

--- api/index.py
+++ api/index.py
@@ -4,33 +4,6 @@ import os
 
 app = Flask(__name__)
 
-model_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "..", "models", "HOF-classifier.pkl"
-)
-scaler_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "..", "models", "HOF-scaler.pkl"
-)
-print(model_path)
-
-# Load the trained model
-model = joblib.load(model_path)
-scaler = joblib.load(scaler_path)
-
-# model_cols = ['All_Star','OWS_advanced', 'BLK_totals',  'FG%_totals',  'PER_advanced', 'MVP', 'ws_seasonal', 'pts_per_g_seasonal', 'DWS_advanced', 'TRB_totals', 'Champ']
-model_cols = [
-    "MVP",
-    "All_Star",
-    "Field_Goal_Percentage",
-    "Total_Rebounds",
-    "Total_Blocks",
-    "Points_Per_Game_Award",
-    "Win_Shares",
-    "Player_Efficiency_Rating",
-    "Offensive_Win_Shares",
-    "Defensive_Win_Shares",
-    "Championships",
-]
-
 @app.route('/')
 def home():
     return 'Hello, World!'
@@ -39,6 +12,32 @@ def home():
 #  methods=["POST"]
 @app.route('/api/flask-predict')
 def flask_predict():
+    model_path = os.path.join(
+        os.path.dirname("../models/HOF-classifier.pkl"
+    )
+    scaler_path = os.path.join(
+        os.path.dirname("../models/HOF-scaler.pkl"
+    )
+    print(model_path)
+    # Load the trained model
+    model = joblib.load(model_path)
+    scaler = joblib.load(scaler_path)
+
+    # model_cols = ['All_Star','OWS_advanced', 'BLK_totals',  'FG%_totals',  'PER_advanced', 'MVP', 'ws_seasonal', 'pts_per_g_seasonal', 'DWS_advanced', 'TRB_totals', 'Champ']
+    model_cols = [
+        "MVP",
+        "All_Star",
+        "Field_Goal_Percentage",
+        "Total_Rebounds",
+        "Total_Blocks",
+        "Points_Per_Game_Award",
+        "Win_Shares",
+        "Player_Efficiency_Rating",
+        "Offensive_Win_Shares",
+        "Defensive_Win_Shares",
+        "Championships",
+    ]
+    
     try:
         # Parse the input values from the request body
         input_values = request.json
